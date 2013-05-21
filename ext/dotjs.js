44582a1cbb3c16ba8304f92a4b2a48588bc1e525
Fuad Saud <fuadksd@gmail.com>
2013-05-21 03:40:06 -0300

    Add basic CSS loading support

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,27 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+$(function() {
+
+  var baseURL = 'https://localhost:3131/';
+  var host = window.location.host.replace('www.', '');
+
+  var error_handler = function() {
+    console.log('no dotjs server found at localhost:3131');
+  };
+
+  // Load the js.
+  $.ajax({
+    url: baseURL + host + '.js',
+    dataType: 'text',
+    success: function(d) { $(function() { eval(d); }); },
+    error: error_handler
+  });
+
+  // Load the css.
+  $.ajax({
+    url: baseURL + host + '.css',
+    dataType: 'text',
+    success: function(d) {
+      $(function() { $('<style></style>').text(d).appendTo($('head')); });
+    },
+    error: error_handler
+  });
+});
